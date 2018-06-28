e0e875925b0c8bc1bd9d064dc04232c4f466f86e
Nick Johnson <njohnson@inbox.lv>
2018-06-27 18:59:52 -0700

    added db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/hello_world/' => 'application#hello_world'
+  get '/hello/:name'  => 'application#hello_world'
 end
