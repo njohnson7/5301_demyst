b432fbf7e8c19889b1ab57a6f08b5516b88792e1
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-24 13:20:18 -0700

    added layouts and partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,10 @@ class CommentsController < ApplicationController
   def create
     @comment = @post.build_comment params[:comment]
     if @comment.save
+      flash[:success] = 'You have successfully created the comment.'
       redirect_to post_path(@post.id)
     else
+      flash.now[:error] = "Comment couldn't be created. Please check the errors."
       render 'posts/show'
     end
   end
