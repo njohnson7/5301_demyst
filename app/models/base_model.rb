373f3e05549f0a028b8582d512d8ade5e3125a27
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-23 19:17:45 -0700

    added custom form tag

--- app/models/base_model.rb
+++ app/models/base_model.rb
@@ -1,6 +1,6 @@
 class BaseModel
   def self.connection
-    db_connection                 = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection = SQLite3::Database.new 'db/development.sqlite3'
     db_connection.results_as_hash = true
     db_connection
   end
